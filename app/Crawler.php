4979ea23b00b455e7b500d1159eaa18aa916139a
Muhammad Faridi <mfaridi@oliversolutions.com>
2022-09-01 14:21:56 -0400

    My Changes

--- app/Crawler.php
+++ app/Crawler.php
@@ -40,6 +40,28 @@ class Crawler
     }
 
     /**
+     * @return void
+     */
+    public function createReport() : void
+    {
+        echo "Pages Crawled: " .$this->pages."<br/>";
+        echo "Unique images: " .count($this->imgStorage)."<br/>";
+        echo "Unique Internal Links: " .count($this->urlStorageInternal)."<br/>";
+        echo "Unique External Links: " .count($this->urlStorageExternal)."<br/>";
+        echo "Average Load Time: " .array_sum($this->avgLoadTime)/count($this->avgLoadTime)."<br/>";
+        echo "Average Word Count: " .array_sum($this->avgWordCount)/count($this->avgWordCount)."<br/>";
+        echo "Average Title Length: " .array_sum($this->avgTitleLength)/count($this->avgTitleLength);
+
+        echo "<h3>Crawled Pages</h3>";
+        echo "<table style='margin:10px 0 0 0;' border='1'>";
+        echo "<thead><tr><td>URL</td><td>HTTP Status</td></tr></thead>";
+        foreach ($this->httpStatusStorage as $item) {
+            echo "<tr><td>".$item['url']. "</td> <td>" .$item['status'] . "</td></tr>";
+        }
+        echo "</table>";
+    }
+
+    /**
      * Triggers the crawling of the pages
      * @param $url
      * @return void
--- index.php
+++ index.php
@@ -4,12 +4,9 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
 require_once "app/Crawler.php";
-require_once "app/Reporting.php";
 
 use app\Crawler\Crawler;
-use app\Crawler\Reporting;
 
 $crawler = new Crawler('https://agencyanalytics.com',5);
-$reporting = new Reporting('https://agencyanalytics.com',5);
-$reporting->createReport();
+$crawler->createReport();
 
