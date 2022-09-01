b15897befbde11ca759c6ea093ec8be98f473361
Muhammad Faridi <mfaridi@oliversolutions.com>
2022-09-01 17:36:12 -0400

    My Changes

--- index.php
+++ index.php
@@ -3,9 +3,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-spl_autoload_register(function($class) {
-    include str_replace('\\', '/', $class) . '.php';
-});
+require_once "src/Crawler.php";
+use app\Crawler\Crawler;
 
 $crawler = new Crawler('https://agencyanalytics.com',5);
 $crawler->crawlPage()->createReport();
