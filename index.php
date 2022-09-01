ffe33405fab44e23eb4899b2b5aee8c08daedb64
Muhammad Faridi <mfaridi@oliversolutions.com>
2022-09-01 17:02:34 -0400

    My Changes

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-require_once "app/Crawler.php";
+require_once "src/Crawler.php";
 use app\Crawler\Crawler;
 
 $crawler = new Crawler('https://agencyanalytics.com',5);
