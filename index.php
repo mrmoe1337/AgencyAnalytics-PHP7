61789d40bf72a802d486998b1d8ac55af836f5a2
Muhammad Faridi <mfaridi@oliversolutions.com>
2022-09-01 17:27:48 -0400

    My Changes

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
 spl_autoload_register(function($class){
-    require_once __DIR___.'/src/'.str_replace('\\','/', $class) . '.php';
+    require_once './src/'.str_replace('\\','/', $class) . '.php';
 });
 
 use src\Crawler\Crawler;
