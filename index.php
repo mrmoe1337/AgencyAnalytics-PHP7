30fbe4100c8b0fe0c36947f1a44d49c4a5f12bc5
Muhammad Faridi <mfaridi@oliversolutions.com>
2022-09-02 22:27:44 -0400

    Some more changes...

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
 
 
+
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
 require 'vendor/autoload.php';
+use Agencyanalytics\App\Crawler;
+
 
 $crawler = new Crawler(
     url: 'https://agencyanalytics.com',
